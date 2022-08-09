f64f5f188bf1d9862bb6c067f578de7035309f40
Levi Barsis <lbarsis@gmail.com>
2022-08-09 12:25:18 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(special='*') {
+    const innerFunction = function(msg) {
+        if (special === '*') {
+            msg = 'a hard worker'
+        } else if (special === '||') {
+            msg = 'a dedicated programmer'
+        }
+        return `You are ${special}${msg}${special}!`;
+    }
+    return innerFunction;
+}
